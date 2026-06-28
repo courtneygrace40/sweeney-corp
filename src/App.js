4e1238b89b65308b73d0be411219e7a18fc7f62c
courtneygrace40 <courtneygrace40@gmail.com>
2026-06-28 19:58:09 -0400

    rendering login maybe?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+// src/App.jsx
+import Login from './components/Login'; // Adjust the path depending on where your file is
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {/* Rendering your login component here */}
+      <Login />
     </div>
   );
 }
