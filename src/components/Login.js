75f03a70c8bbf3ecd5001394f98341a42565f191
courtneygrace40 <courtneygrace40@gmail.com>
2026-06-28 20:01:35 -0400

    hoping and praying

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ import './Login.css';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setErrors] = useState('');
+  const [errors, setErrors] = useState('');
 
   const validateForm = () => {
     const newErrors = {};
